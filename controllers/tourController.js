5979ebff68e4b82cf466de6e8b1555792319ceae
ThaoTTP <dreemmurrchara@gmail.com>
2023-12-13 14:29:19 +0700

    delete log

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,9 +57,7 @@ exports.aliasTopTours = (req, res, next) => {
 };
 exports.getMyTours = catchAsync(async (req, res, next) => {
   // 1) Find all bookings
-  console.log(req.user.id);
   const bookings = await Booking.find({ user: req.user.id });
-  if (!bookings) console.log('aaaaaaaa');
   // 2) Find tours with the returned IDs
   const tourIDs = bookings.map((el) => el.tour);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
